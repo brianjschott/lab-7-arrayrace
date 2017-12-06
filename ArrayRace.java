25617ae8790a695ffedbf8258f84652e2d56aecf
Brian Schott <brian@bronxsoftware.org>
2017-12-06 07:47:30 -0500

    Update ArrayRace.java

--- ArrayRace.java
+++ ArrayRace.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 
 public class ArrayRace
 {
-	private int one;
-	private int two;
-	private int oneCount;
-	private int twoCount;
-	private int[] track;
-	private int turn;
-	private int winner;
+	private int one; //should be the integer 1
+	private int two; //should be the integer 2
+	private int oneCount; //how many times player one gets to the end
+	private int twoCount; //how many times player two gets to the end
+	private int[] track; //the track
+	private int turn; //the value represents whose turn it is
+	private int winner; //the value representing the winner
 	
-	// C
+	// Assigns track to be an array with values, and initializes all other values
 	public ArrayRace()
 	{
 	}
 	
 	public boolean turn()
 	{
+		
 		return false;
 	}
 	
+	// returns name of winner and a congratulatory message (only call this method if someone's count is >= 5)
 	public String getWinner()
 	{
+		
 		return "";
-  } 
+  	} 
   	
   public String toString()
   {
