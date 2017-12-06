482ef8195da5b069fc463dd331fdfef713f1a405
Brian Schott <brian@bronxsoftware.org>
2017-12-06 07:38:19 -0500

    Create ArrayRace.java

---
+++ ArrayRace.java
@@ -0,0 +1,34 @@
+import java.lang.System;
+import java.util.Arrays;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class TheRace
+{
+	private int one;
+	private int two;
+	private int oneCount;
+	private int twoCount;
+	private int[] track;
+	private int turn;
+	private int winner;
+	
+	public TheRace()
+	{
+	}
+	
+	public boolean turn()
+	{
+		return false;
+	}
+	
+	public String getWinner()
+	{
+		return "";
+  } 
+  	
+  public String toString()
+  {
+  	return Arrays.toString( track ) + " one count = " + oneCount + " twoCount = " + twoCount;
+  }
+}
